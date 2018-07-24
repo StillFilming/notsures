8e34ff82b7b9282ed00e6f8c8e3b3c459c60d026
stillfilming <ylozitskiy@gmail.com>
2018-07-24 12:09:05 -0400

    .

---
+++ index.js
@@ -0,0 +1,31 @@
+const unirest = require('unirest')
+const htmlToText = require('html-to-text')
+const async = require("async")
+const cheerio = require('cheerio')
+const express = require('express')
+const http = require('http')
+const https = require('https')
+const events = require('events')
+const path = require('path')
+const FetchStream = require('fetch').FetchStream
+const fs = require('fs')
+const PORT = process.env.PORT || 5000
+
+express()
+	.use(express.static(path.join(__dirname, 'public')))
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.get('/', (req, res) => res.render('pages/index'))
+
+	.get('/gossip', (req, res) => {
+		
+		let pageView = "Start"
+
+		let domains = "google.com,facebook.com,youtube.com"			
+		domains = domains.split(",")
+		
+
+		res.send(pageView)		
+		
+	})
+	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
