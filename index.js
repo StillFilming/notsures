23e339908e86bef8e6d2037234855b17c7b76224
stillfilming <ylozitskiy@gmail.com>
2018-07-24 12:26:15 -0400

    .

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const FetchStream = require('fetch').FetchStream
 const fs = require('fs')
 const PORT = process.env.PORT || 5000
 
+let full_url = ""
+
 express()
 	.use(express.static(path.join(__dirname, 'public')))
 	.set('views', path.join(__dirname, 'views'))
@@ -19,16 +21,36 @@ express()
 
 	.get('/gossip', (req, res) => {
 		
-		let pageView = ""
+		let pageView = "START"
 
 		let domains = "google.com,facebook.com"			
 		domains = domains.split(",")
 		
 		async.eachLimit(domains, 2, function (url, callback) {
 			pageView = pageView + "\n" + url
+			full_url = "http://www."+url
+			
+			unirest.get(full_url).maxRedirects(2).timeout(100000).end(function (result) {
+				if(result.error){
+					pageView = pageView+ "\n" + "-------------- Failed: " + result.error
+				} else {
+					html_content = result.body
+					var clean_text = htmlToText.fromString(html_content, {
+						wordwrap: null,
+						ignoreHref: true,
+						ignoreImage: true,
+						preserveNewlines: true
+					})
+					clean_text = clean_text.replace(/[^\x00-\x7F]/g, " ")
+					clean_text = clean_text.replace(/\s+/g, " ")
+					pageView = pageView+ "\n" + "++++ Collected: "+url + " ||||||||| " + clean_text.slice(0,100)
+				}
+			})
+			callback()
+			
 		}, function (err) {
 			if(err){
-				pageView = pageView + "Failed: " + url
+				pageView = pageView+ "\n" + "Failed: " + url
 			}
 		})
 
